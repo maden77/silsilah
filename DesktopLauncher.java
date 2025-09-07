b2fd24832dd28b9522c9fb7064fab8dbee248513
@maden <166928262+maden77@users.noreply.github.com>
2025-09-07 08:05:36 +0700

    Create DesktopLauncher.java

---
+++ DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdxgame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Ular Tangga Game");
+        config.setWindowedMode(800, 600);
+        config.setResizable(false);
+        new Lwjgl3Application(new MyGdxGame(), config);
+    }
+}
